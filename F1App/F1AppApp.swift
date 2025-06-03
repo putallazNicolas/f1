545110c29c142b0bbff5811380dd5dc52c89b96c
Fernando Putallaz <fputallaz@rccl.com>
2025-06-03 15:26:00 -0300

    Initial Commit

---
+++ F1App/F1AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1AppApp.swift
+//  F1App
+//
+//  Created by Fernando Jorge Putallaz on 03/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct F1AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ F1AppTests/F1AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  F1AppTests.swift
+//  F1AppTests
+//
+//  Created by Fernando Jorge Putallaz on 03/06/2025.
+//
+
+import Testing
+@testable import F1App
+
+struct F1AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
